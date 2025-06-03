0f7389ea42d24df72728a5bf3328c03412d569d7
Deni1801 <deni.tric01@gmail.com>
2025-06-03 11:03:03 +0200

    Update gatk3_reallignment.bash

--- scripts/gatk3_reallignment.bash
+++ scripts/gatk3_reallignment.bash
@@ -1,7 +1,7 @@
 % Reallignment of control
-gatk3 -T RealignerTargetCreator -R ../Annotations/human_g1k_v37.fasta -I Control.sorted.bam -o realigner.intervals -L DNA_Repair_Genes.bed
-gatk3 -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I Control.sorted.bam -targetIntervals realigner.intervals -o Control.sorted.realigned.bam -L DNA_Repair_Genes.bed
+gatk3 -T RealignerTargetCreator -R ../Annotations/human_g1k_v37.fasta -I Control.sorted.bam -o control.realigner.intervals -L DNA_Repair_Genes.bed
+gatk3 -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I Control.sorted.bam -targetIntervals control.realigner.intervals -o Control.sorted.realigned.bam -L DNA_Repair_Genes.bed
 
 % Reallignment of tumor
-gatk3 -T RealignerTargetCreator -R ../Annotations/human_g1k_v37.fasta -I Tumor.sorted.bam -o realigner.intervals -L DNA_Repair_Genes.bed
-gatk3 -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I Tumor.sorted.bam -targetIntervals realigner.intervals -o Tumor.sorted.realigned.bam -L DNA_Repair_Genes.bed
+gatk3 -T RealignerTargetCreator -R ../Annotations/human_g1k_v37.fasta -I Tumor.sorted.bam -o tumor.realigner.intervals -L DNA_Repair_Genes.bed
+gatk3 -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I Tumor.sorted.bam -targetIntervals tumor.realigner.intervals -o Tumor.sorted.realigned.bam -L DNA_Repair_Genes.bed
