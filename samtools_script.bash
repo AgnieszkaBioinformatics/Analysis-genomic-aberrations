e13396de7eec96c9b99b29d441066bd1b16ababa
amicobudino <161333878+amicobudino@users.noreply.github.com>
2025-06-03 10:14:17 +0200

    Update samtools_script.bash

--- samtools_script.bash
+++ samtools_script.bash
@@ -1,2 +1,11 @@
-% sorting the bam files
+% sorting the bam files (control and tumor)
+
 samtools sort Control.bam > Control.sorted.bab
+samtools sort Tumor.bam > Tumor.sorted.bam
+
+% Index the Sorted BAM File
+
+samtools index Control.sorted.bam
+samtools index Tumor.sorted.bam
+
+
