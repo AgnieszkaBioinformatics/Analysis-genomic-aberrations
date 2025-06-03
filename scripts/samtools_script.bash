780fa02751fbc891f77d0de405a6e2262b901adc
amicobudino <161333878+amicobudino@users.noreply.github.com>
2025-06-03 10:40:02 +0200

    Update samtools_script.bash

--- scripts/samtools_script.bash
+++ scripts/samtools_script.bash
@@ -8,4 +8,15 @@ samtools sort Tumor.bam > Tumor.sorted.bam
 samtools index Control.sorted.bam
 samtools index Tumor.sorted.bam
 
+% count the reads in the bam files
 
+samtools view -c Control.sorted.bam
+% out: 19720171
+samtools view -c Tumor.sorted.bam
+% out: 15039503
+
+% count reads that map to reverse strand - the `-f 16` allows me to use the bitwise FLAG to filter the bam file
+
+samtools view -c -f 16 Control.sorted.bam
+
+samtools view -c -f 16 Tumor.sorted.bam
