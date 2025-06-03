e8b287a7fb834b42876fbf3e3dc8456c1f09c3a8
Deni1801 <deni.tric01@gmail.com>
2025-06-03 10:43:18 +0200

    Update gatk3_reallignment.bash

--- scripts/gatk3_reallignment.bash
+++ scripts/gatk3_reallignment.bash
@@ -1,1 +1,2 @@
 gatk3 -T RealignerTargetCreator -R ../Annotations/human_g1k_v37.fasta -I Control.sorted.bam -o realigner.intervals -L DNA_Repair_Genes.bed
+gatk3 -T IndelRealigner -R ../Annotations/human_g1k_v37.fasta -I Control.sorted.bam -targetIntervals realigner.intervals -o Control.sorted.realigned.bam -L DNA_Repair_Genes.bed
