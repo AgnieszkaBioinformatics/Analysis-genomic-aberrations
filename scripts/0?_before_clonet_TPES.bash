dff4b350935a1761de8344a0cdb880876430fa3d
Deni1801 <deni.tric01@gmail.com>
2025-06-14 10:07:57 +0200

    Update 0?_before_clonet_TPES.bash

--- scripts/0?_before_clonet_TPES.bash
+++ scripts/0?_before_clonet_TPES.bash
@@ -1,5 +1,5 @@
 % Before clonet 
-gatk3 -T ASEReadCounter -R ../Annotations/human_g1k_v37.fasta -o Normal.csv -I Control.sorted.realigned.recalibrated.bam -sites Control.het.vcf -U ALLOW_N_CIGAR_READS -minDepth 20 --minMappingQuality 20 --minBaseQuality 20
+gatk3 -T ASEReadCounter -R ../Annotations/human_g1k_v37.fasta -o Control.csv -I Control.sorted.realigned.recalibrated.bam -sites Control.het.vcf -U ALLOW_N_CIGAR_READS -minDepth 20 --minMappingQuality 20 --minBaseQuality 20
 gatk3 -T ASEReadCounter -R ../Annotations/human_g1k_v37.fasta -o Tumor.csv -I Tumor.sorted.realigned.recalibrated.bam -sites Control.het.vcf -U ALLOW_N_CIGAR_READS -minDepth 20 --minMappingQuality 20 --minBaseQuality 20
 
 % Before TPES
