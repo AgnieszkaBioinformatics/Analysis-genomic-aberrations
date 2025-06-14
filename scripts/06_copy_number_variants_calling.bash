9618a218df0c6df0d28f64d2b60461b1631966f2
amicobudino <161333878+amicobudino@users.noreply.github.com>
2025-06-14 12:27:29 +0200

    Update 06_copy_number_variants_calling.bash

--- scripts/06_copy_number_variants_calling.bash
+++ scripts/06_copy_number_variants_calling.bash
@@ -1,5 +1,9 @@
 # Generate mpileup for control and tumor samples
-samtools mpileup -f ./data/annotations/human_g1k_v37.fasta ./out_duplicates_removal/Control.sorted.realigned.recalibrated.dedup.bam ./out_duplicates_removal/Tumor.sorted.realigned.recalibrated.dedup.bam | varscan copynumber --output-file ./out_scnv/scna --mpileup 1
+samtools mpileup -f ./data/annotations/human_g1k_v37.fasta \
+  ./out_duplicates_removal/Control.sorted.realigned.recalibrated.dedup.bam \
+  ./out_duplicates_removal/Tumor.sorted.realigned.recalibrated.dedup.bam \
+  | varscan copynumber --output-file ./out_scnv/scna --mpileup 1
 
 # segment the copy number data assigning number states (gain/loss/normal) to genomic regions
-varscan copyCaller ./out_scnv/scna.copynumber --output-file ./out_scnv/scna.copynumber.calledsegments
+varscan copyCaller ./out_scnv/scna.copynumber \
+  --output-file ./out_scnv/scna.copynumber.calledsegments
